9762ac1eba106cf06f357a0531b35bf858f69cf9
Gagandeep Thind <155720950+gagandeeps-thind@users.noreply.github.com>
2025-11-27 22:18:31 -0700

    Update avatar image path in siteContent.js

--- src/data/siteContent.js
+++ src/data/siteContent.js
@@ -3,7 +3,7 @@
 
 export const about = {
   name: 'Prashasti Kapoor',
-  avatar: '/dp.jpg', // put image in /public/
+  avatar: '/ppp.jpg', // put image in /public/
   tagline: '“Engineering Excellence Through Testing.”',
   description: 'Quality Assurance Engineer | Cypress | Appium | Selenium | Postman | SDLC',
   links: {
